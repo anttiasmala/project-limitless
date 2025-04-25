20ba462222051382df59774d2324b21cd984facb
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-25 17:45:01 +0300

    Add _app.tsx, _document.tsx and index.tsx into pages folder

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-screen w-full">
+      <div className="flex h-full w-full justify-center bg-gray-500">
+        <div className="w-full bg-blue-500 md:w-1/2"></div>
+      </div>
+    </main>
+  );
+}
