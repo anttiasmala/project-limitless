03cd68c0916fa202e53805d7bad2c03d2973a0fc
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-26 09:06:54 +0300

    Clicking Project Limitless text will bring back to main page

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export function Topbar() {
+  const router = useRouter();
+
   return (
     <div className="relative flex justify-center">
       <div className="absolute font-bold text-black text-shadow-lg">
-        Project Limitless Vesiosuuskunta
+        <button
+          onClick={() => {
+            router.push('/').catch((e) => console.error(e));
+          }}
+        >
+          Project Limitless Vesiosuuskunta
+        </button>
       </div>
       <Image
         src={'/water.png'}
