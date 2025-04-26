a382b19f2d4170a19424f5122b37023e2043ac81
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-26 09:12:50 +0300

    Add regexPatterns.ts, types.ts and zodSchemas.ts from another project

---
+++ shared/types.ts
@@ -0,0 +1,60 @@
+import { z } from 'zod';
+import {
+  fullUserSchema,
+  userSchema,
+  getUserSchema,
+  fullSessionSchema,
+  sessionSchema,
+  getSessionSchema,
+  createSessionSchema,
+  invalidSessionResultSchema,
+  validSessionResultSchema,
+  frontendSessionSchema,
+} from './zodSchemas';
+
+// USER
+
+export type FullUser = z.infer<typeof fullUserSchema>;
+
+export type User = z.infer<typeof userSchema>;
+
+export type GetUser = z.infer<typeof getUserSchema>;
+
+// SESSION
+
+export type FullSession = z.infer<typeof fullSessionSchema>;
+
+export type Session = z.infer<typeof sessionSchema>;
+
+export type GetSession = z.infer<typeof getSessionSchema>;
+
+export type FrontendSession = z.infer<typeof frontendSessionSchema>;
+
+export type CreateSession = z.infer<typeof createSessionSchema>;
+
+type validSessionResult = z.infer<typeof validSessionResultSchema>;
+
+type invalidSessionResult = z.infer<typeof invalidSessionResultSchema>;
+
+export type GetUserAndSessionResult = validSessionResult | invalidSessionResult;
+
+export type DatabaseAdapter = {
+  createSession: (sessionData: CreateSession) => Promise<Session | null>;
+  deleteSession: (sessionUUID: string) => Promise<void>;
+
+  //prettier-ignore
+
+  /* No use for these? */
+  //getSession: (sessionUUID: string) => Promise<Session | null>;
+  //getUserFromSession: (sessionUUID: string) => Promise<User | null>; // potentially a dangerous function
+  //getUserAndSessions: (userUUID: string) => Promise<[Session[], User] | null>; // gets the user and ALL the sessions
+
+  // prettier-ignore
+  //prettier-ignore
+  getUserAndSession: (sessionUUID: string) => Promise<GetUserAndSessionResult>; // gets the user and ONLY ONE session
+  getUserSessions: (userUUID: string) => Promise<Session[]>; // gets all the sessions belonging to a ONE user
+  // prettier-ignore
+  updateSessionExpirationDate: (sessionUUID: string, newSessionExpirationDate: Date) => Promise<void>;
+  deleteUserSessions: (userUUID: string) => Promise<void>; // deletes all the sessions belonging to a user
+  deleteExpiredSessions: () => Promise<void>;
+};
---
+++ shared/zodSchemas.ts
@@ -0,0 +1,116 @@
+import { z } from 'zod';
+import { emailRegex, passwordRegex } from './regexPatterns';
+
+export const uuidSchema = z
+  .string({ message: 'Invalid UUID! It should be given as a string!' })
+  .uuid('UUID pattern was invalid!');
+
+export const dateSchema = z
+  .string()
+  .min(1, 'Date is mandatory!')
+  .datetime({ message: 'Given date is invalid!' })
+  .pipe(z.coerce.date());
+
+const firstNameSchema = z
+  .string()
+  .min(1, 'Etunimi on pakollinen!')
+  .max(128, 'Etunimi on liian pitkä, maksimipituus on 128 merkkiä');
+
+const lastNameSchema = z
+  .string()
+  .min(1, 'Sukunimi on pakollinen!')
+  .max(128, 'Sukunimi on liian pitkä, maksimipituus on 128 merkkiä');
+
+export const emailSchema = z
+  .string()
+  .min(1, 'Sähköposti on pakollinen!')
+  .max(128, 'Sähköposti on liian pitkä, maksimipituus on 128 merkkiä')
+  .regex(emailRegex, 'Sähköposti on virheellinen')
+  .transform((value) => value.toLowerCase());
+
+const passwordSchema = z
+  .string()
+  .min(1, 'Salasana on pakollinen!')
+  .max(128, 'Salasana on liian pitkä, maksimipituus on 128 merkkiä')
+  .regex(
+    passwordRegex,
+    'Salasanan täytyy olla vähintään 8 merkkiä pitkä, maksimissaan 128 merkkiä pitkä, sekä sisältää vähintään yksi iso kirjain, yksi pieni kirjain, yksi numero ja yksi erikoismerkki!',
+  );
+
+// USER
+
+/** Contains ALL the rows in database */
+export const fullUserSchema = z.object({
+  id: z.number().min(1, 'ID is mandatory!'),
+  uuid: uuidSchema,
+  firstName: firstNameSchema,
+  lastName: lastNameSchema,
+  email: emailSchema,
+  password: passwordSchema,
+  vesiosuuskuntaName: z
+    .string()
+    .min(1, 'Name for Vesiosuuskunta is mandatory!'),
+  role: z.literal('USER').or(z.literal('ADMIN')),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+/** Contains mandatory data to specify a user. Such as email, first name, etc */
+export const userSchema = fullUserSchema.pick({
+  email: true,
+  firstName: true,
+  lastName: true,
+  vesiosuuskuntaName: true,
+});
+
+/** Schema to be used when wanting details of user. It does not have password etc for a security reason */
+export const getUserSchema = fullUserSchema
+  .pick({
+    uuid: true,
+    createdAt: true,
+    updatedAt: true,
+    role: true,
+  })
+  .merge(userSchema);
+
+// SESSION
+
+export const fullSessionSchema = z.object({
+  uuid: uuidSchema,
+  userUUID: uuidSchema,
+  expiresAt: z.date(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  isLoggedIn: z.boolean(),
+});
+
+export const getSessionSchema = fullSessionSchema.pick({
+  uuid: true,
+  userUUID: true,
+  expiresAt: true,
+  isLoggedIn: true,
+});
+
+export const sessionSchema = getSessionSchema;
+
+export const frontendSessionSchema = getSessionSchema.extend({
+  fresh: z.boolean(),
+});
+
+export const createSessionSchema = getSessionSchema.pick({
+  uuid: true,
+  userUUID: true,
+  expiresAt: true,
+});
+
+export const validSessionResultSchema = z.object({
+  status: z.literal('valid'),
+  databaseSession: sessionSchema,
+  databaseUser: getUserSchema,
+});
+
+export const invalidSessionResultSchema = z.object({
+  status: z.literal('invalid'),
+  databaseSession: z.null(),
+  databaseUser: z.null(),
+});
