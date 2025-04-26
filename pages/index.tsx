afdc3025b623f210011a376a6f92aaecfe9cd171
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-26 06:47:55 +0300

    Add TopBar 'private' component and add some styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,34 @@
+import Image from 'next/image';
+import { Button } from '~/components/Button';
+
 export default function Home() {
   return (
     <main className="h-screen w-full">
       <div className="flex h-full w-full justify-center bg-gray-500">
-        <div className="w-full bg-blue-500 md:w-1/2"></div>
+        <div className="w-full bg-blue-500 md:w-1/2">
+          <Topbar></Topbar>
+          <div className="flex flex-col items-center">
+            <Button className="mt-1">Jäsenet</Button>
+          </div>
+        </div>
       </div>
     </main>
+  );
+}
+
+function Topbar() {
+  return (
+    <div className="relative flex justify-center">
+      <div className="absolute font-bold text-black text-shadow-lg">
+        Project Limitless Vesiosuuskunta
+      </div>
+      <Image
+        src={'/water.png'}
+        alt="waterdrop"
+        className="h-20 w-full md:h-40"
+        width={600}
+        height={1}
+      />
+    </div>
   );
 }
