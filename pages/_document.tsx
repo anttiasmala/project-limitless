1843da060308da5b57840f82a7d07840090a7199
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-26 07:25:03 +0300

    Move bg-gray-500 to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-black antialiased">
+      <body className="bg-gray-500 antialiased">
         <Main />
         <NextScript />
       </body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Button } from '~/components/Button';
 export default function Home() {
   return (
     <main className="h-screen w-full">
-      <div className="flex h-full w-full justify-center bg-gray-500">
+      <div className="flex h-full w-full justify-center">
         <div className="w-full bg-blue-500 md:w-1/2">
           <Topbar></Topbar>
           <div className="flex flex-col items-center">
