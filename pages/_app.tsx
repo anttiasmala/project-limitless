969b841a51acafc86172094043a84cc7d1c47eda
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-26 19:18:38 +0300

    Add QueryClient and QueryClientProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
 import '~/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <Head>
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, interactive-widget=resizes-visual"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, interactive-widget=resizes-visual"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </div>
+    </QueryClientProvider>
   );
 }
