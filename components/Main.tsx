01fe6cbd4fa7b1be44ce42c25852b32697aadcb4
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-26 08:31:48 +0300

    Add Topbar to the Main component

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,10 +1,14 @@
 import { HTMLAttributes } from 'react';
+import { Topbar } from './Topbar';
 
 export function Main({ children }: HTMLAttributes<HTMLDivElement>) {
   return (
     <main className="h-screen w-full">
       <div className="flex h-full w-full justify-center">
-        <div className="w-full bg-blue-500 md:w-1/2">{children}</div>
+        <div className="w-full bg-gray-200 md:w-1/2">
+          <Topbar />
+          {children}
+        </div>
       </div>
     </main>
   );
