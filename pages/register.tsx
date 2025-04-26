2707b531984c82be7dbdb68080f01f0db9b3d6ec
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-26 07:23:47 +0300

    Add register.tsx

---
+++ pages/register.tsx
@@ -0,0 +1,9 @@
+import { Main } from '~/components/Main';
+
+export default function Register() {
+  return (
+    <Main>
+      <div className="flex flex-col items-center">a</div>
+    </Main>
+  );
+}
