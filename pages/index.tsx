383b1f4c5f84613393d03ad9237d3887434ddcbe
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-26 13:29:46 +0300

    Add Main component to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,17 @@
 import { Button } from '~/components/Button';
+import { Main } from '~/components/Main';
 import { Topbar } from '~/components/Topbar';
 import { getServerSideProps } from '~/utils/getServerSideProps';
 
 // this checks login status
-export { getServerSideProps };
+//export { getServerSideProps };
 
 export default function Home() {
   return (
-    <main className="h-screen w-full">
-      <div className="flex h-full w-full justify-center">
-        <div className="w-full bg-blue-500 md:w-1/2">
-          <Topbar></Topbar>
-          <div className="flex flex-col items-center">
-            <Button className="mt-1">Jäsenet</Button>
-          </div>
-        </div>
+    <Main>
+      <div className="flex flex-col items-center">
+        <Button className="mt-1">Jäsenet</Button>
       </div>
-    </main>
+    </Main>
   );
 }
