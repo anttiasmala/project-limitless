8fa90e5d0216a8ad3dcd3771422e61a1070cfa84
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-27 11:23:17 +0300

    Add Prisma error handling from Lahjalista project: https://github.com/samuliasmala/lahjalista

--- backend/handleError.ts
+++ backend/handleError.ts
@@ -1,10 +1,26 @@
 import { NextApiResponse } from 'next';
 import { HttpError } from './HttpError';
 import { ZodError } from 'zod';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 export function handleError(res: NextApiResponse, e: unknown) {
   if (e instanceof HttpError) {
-    return res.status(e.httpStatusCode).send(e.message);
+    return res.status(e.httpStatusCode ?? 400).send(e.message);
+  }
+
+  if (e instanceof PrismaClientKnownRequestError) {
+    if (e.code === 'P2025') {
+      const modelName =
+        typeof e.meta?.modelName === 'string' ? e.meta?.modelName : 'Record';
+      return res.status(404).send(`${modelName} was not found on the server!`);
+    }
+    if (e.code === 'P2002') {
+      const targetName =
+        Array.isArray(e.meta?.target) && typeof e.meta.target[0] === 'string'
+          ? e.meta.target[0]
+          : 'Record';
+      return res.status(400).send(`${targetName} was not unique!`);
+    }
   }
 
   if (e instanceof ZodError) {
