013cd6a8db5c1084be233349db97ac2a35a57b36
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-27 10:38:07 +0300

    Add functionality for registering. In /api/auth/register.ts

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,12 +1,45 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { handleError } from '~/backend/handleError';
 import { HttpError } from '~/backend/HttpError';
+import { CreateUser } from '~/shared/types';
+import { createUserSchema } from '~/shared/zodSchemas';
+import prisma from '~/prisma';
+import { hashPassword } from '~/backend/utils';
 
-export default function Register(req: NextApiRequest, res: NextApiResponse) {
+export default async function Register(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   try {
-    console.log(req.method);
+    if (req.method !== 'POST') {
+      throw new HttpError('Invalid request method!', 405);
+    }
+    const validateRequestBody = createUserSchema.parse(req.body);
+
+    await createUser(validateRequestBody);
+
     res.status(200).end();
-    return;
   } catch (e) {
-    new HttpError('Error occured!', 400);
+    handleError(res, e);
   }
+}
+
+async function createUser(userData: CreateUser) {
+  const hashedPassword = await hashPassword(userData.password);
+  const parsedUserData = createUserSchema.parse({
+    ...userData,
+    password: hashedPassword,
+  });
+
+  await prisma.user.create({
+    data: parsedUserData,
+    select: {
+      uuid: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
 }
