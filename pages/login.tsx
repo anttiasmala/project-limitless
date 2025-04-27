2dc97d17d285522dca5ae45f1bf9c6c1f47b01b3
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-27 11:55:19 +0300

    Add Email and Password useStates. Add some functionality how to store the data etc

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import { Button } from '~/components/Button';
 import { Input } from '~/components/Input';
 import LinkElement from '~/components/LinkElement';
 import { Main } from '~/components/Main';
 
 export default function Register() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   return (
     <Main>
       <div className="flex flex-col items-center">
@@ -14,12 +18,19 @@ export default function Register() {
             <Input
               name="email"
               placeholder="matti.meikalainen@email.com"
-              className=""
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
           <div className="mt-2 flex flex-col">
             <label htmlFor="password">Salasana:</label>
-            <Input name="password" placeholder="**************" />
+            <Input
+              name="password"
+              placeholder="**************"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
           </div>
           <Button
             className="mt-10 w-72 min-w-72 rounded-md p-3 text-black hover:text-blue-500"
