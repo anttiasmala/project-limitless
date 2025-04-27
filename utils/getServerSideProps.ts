bc4cf93c733f8fddd14808c79980bb37b76a7833
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-27 18:41:55 +0300

    Lend getServerSideProps from Lahjalista project

--- utils/getServerSideProps.ts
+++ utils/getServerSideProps.ts
@@ -1,15 +1,44 @@
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import { auth, validateRequest } from '~/backend/auth/vesiosuuskunta-auth';
 import { User } from '~/shared/types';
 import { getUserSchema } from '~/shared/zodSchemas';
 
 export async function getServerSideProps(
   context: GetServerSidePropsContext,
 ): Promise<GetServerSidePropsResult<{ user: User }>> {
-  // Return user for now to login page
+  const cookieData = await validateRequest(context.req, context.res);
+
+  if (
+    !cookieData.user ||
+    !cookieData.session ||
+    !cookieData.session.isLoggedIn
+  ) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/login',
+      },
+    };
+  }
+  const returnThis = getUserSchema.safeParse(cookieData.user);
+  if (returnThis.error) {
+    await auth.deleteSession(cookieData.session.uuid);
+
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/login',
+      },
+    };
+  }
+
   return {
-    redirect: {
-      permanent: false,
-      destination: '/login',
+    props: {
+      // getStaticProps can only return plain objects (i.e., objects with
+      // primitive values). This is why we use JSON.parse and JSON.stringify
+      // to convert the object to a plain object (specifically here we convert
+      // the user.createdAt Date object to a string).
+      user: JSON.parse(JSON.stringify(returnThis.data)) as User,
     },
   };
 }
