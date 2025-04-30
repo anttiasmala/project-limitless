1fee874ac000ad858b3b3b3eaecdaedbc3ab9eb0
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-30 11:02:20 +0300

    Add a button that brings to vesiosuuskunta site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
 import { InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/router';
 import { Button } from '~/components/Button';
 import { Main } from '~/components/Main';
 import { Topbar } from '~/components/Topbar';
@@ -11,10 +14,19 @@ export { getServerSideProps };
 export default function Home({
   user,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+
   return (
     <Main user={user}>
       <div className="flex flex-col items-center">
-        <Button className="mt-1">Jäsenet</Button>
+        <Button
+          className="mt-1"
+          onClick={() => {
+            router.push('/vesiosuuskunta').catch((e) => console.error(e));
+          }}
+        >
+          Vesiosuuskunta
+        </Button>
       </div>
     </Main>
   );
