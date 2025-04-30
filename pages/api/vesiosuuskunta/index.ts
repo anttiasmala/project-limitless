bb9184d65d9046d4f0d6d85319086edb5a2eb343
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-30 14:00:50 +0300

    Add functionality for GET request inside /api/vesiosuuskunta/index.ts

--- pages/api/vesiosuuskunta/index.ts
+++ pages/api/vesiosuuskunta/index.ts
@@ -2,10 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next/types';
 import { requireLogin } from '~/backend/auth/vesiosuuskunta-auth';
 import { handleError } from '~/backend/handleError';
 import { HttpError } from '~/backend/HttpError';
+import { GetUser } from '~/shared/types';
+import prisma from '~/prisma';
 
 const HANDLER: Record<
   string,
-  (req: NextApiRequest, res: NextApiResponse) => Promise<void>
+  (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    userData: GetUser,
+  ) => Promise<void>
 > = {
   GET: handleGET,
   POST: handlePOST,
@@ -20,7 +26,7 @@ export default async function handleVesiosuuskuntaRequest(
 
     const reqHandler = req.method !== undefined && HANDLER[req.method];
     if (reqHandler) {
-      await reqHandler(req, res);
+      await reqHandler(req, res, userData);
     } else {
       throw new HttpError(
         `${req.method} is not a valid method. Only GET and POST requests are valid!`,
@@ -32,8 +38,21 @@ export default async function handleVesiosuuskuntaRequest(
   }
 }
 
-async function handleGET(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).end();
+async function handleGET(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  userData: GetUser,
+) {
+  const vesiosuuskunnat = await prisma.vesiosuuskunta.findMany({
+    where: {
+      ownerUUID: userData.uuid,
+    },
+    omit: {
+      id: true,
+    },
+  });
+
+  res.status(200).json(vesiosuuskunnat);
   return;
 }
 
