cface34ec946dde3f113def63f9a749ff6b1b91b
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-04-30 15:37:36 +0300

    Add createVesiosuuskunta into MUTATE_AND_QUERY_KEYS

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,6 +2,7 @@ export const MUTATION_AND_QUERY_KEYS = {
   REGISTER: ['register'],
   LOGIN: ['login'],
   LOGOUT: ['logout'],
+  CREATE_VESIOSUUSKUNTA: ['createVesiosuuskunta'],
 
   // QUERIES
   VESIOSUUSKUNNAT: ['vesiosuuskunnat'],
