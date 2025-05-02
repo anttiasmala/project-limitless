0813396458df4eb423c632b74f142a7b15834e9f
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-02 08:37:07 +0300

    Add Tanstack query. Uses now real data instead of 'hardcoded' data

--- pages/vesiosuuskunta/[uuid]/members.tsx
+++ pages/vesiosuuskunta/[uuid]/members.tsx
@@ -3,7 +3,7 @@ import { HTMLAttributes, useEffect, useRef, useState } from 'react';
 import { Button } from '~/components/Button';
 import { Main } from '~/components/Main';
 import { Topbar } from '~/components/Topbar';
-import { GetUser, GetVesiosuuskunta, User } from '~/shared/types';
+import { GetMember, GetUser, GetVesiosuuskunta, User } from '~/shared/types';
 import { getServerSideProps } from '~/utils/getServerSideProps';
 import {
   arrayOfNames_debug as arrayOfNames,
@@ -32,7 +32,14 @@ export default function Home({
   const [showCreateNewMemberModal, setShowCreateNewMemberModal] =
     useState<boolean>(false);
 
+  const [pageUUID, setPageUUID] = useState('');
+
   useEffect(() => {
+    setPageUUID(
+      window.location.pathname.match(/vesiosuuskunta\/(.+?)\/members/)?.[1] ??
+        '',
+    );
+    refetch();
     document.body.classList.remove('bg-gray-500');
     document.body.classList.add('bg-gray-200');
     return () => {
@@ -40,6 +47,22 @@ export default function Home({
       document.body.classList.add('bg-gray-500');
     };
   }, []);
+
+  const { data: members, refetch } = useQuery({
+    queryKey: MUTATION_AND_QUERY_KEYS.MEMBERS,
+    queryFn: async () => {
+      return (await axios.get(`/api/vesiosuuskunta/${pageUUID}/members`))
+        .data as GetMember[];
+    },
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    retry: false,
+    enabled: false,
+  });
+
+  useEffect(() => {
+    console.log(members);
+  }, [members]);
 
   return (
     <main className="h-screen w-screen">
@@ -63,155 +86,169 @@ export default function Home({
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     Sukunimi
                   </TitleParagraph>
-                  {arrayOfNames.map((v, i) => {
-                    return (
-                      <p key={`${v.lastName}${i}`} className="static">
-                        {v.lastName}
-                      </p>
-                    );
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return (
+                        <p key={`${v.lastName}${i}`} className="static">
+                          {v.lastName}
+                        </p>
+                      );
+                    })}
                 </div>
                 <div>
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     Etunimet
                   </TitleParagraph>
 
-                  {arrayOfNames.map((v, i) => {
-                    return <p key={`${v.firstName}${i}`}>{v.firstName}</p>;
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return <p key={`${v.firstName}${i}`}>{v.firstName}</p>;
+                    })}
                 </div>
                 <div>
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     Katuosoite
                   </TitleParagraph>
 
-                  {arrayOfNames.map((v, i) => {
-                    return (
-                      <p
-                        key={`${v.streetAddress}${i}`}
-                        className="mr-3 lg:mr-0"
-                      >
-                        {v.streetAddress}
-                      </p>
-                    );
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return (
+                        <p
+                          key={`${v.streetAddress}${i}`}
+                          className="mr-3 lg:mr-0"
+                        >
+                          {v.streetAddress}
+                        </p>
+                      );
+                    })}
                 </div>
                 <div>
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     Postinro
                   </TitleParagraph>
 
-                  {arrayOfNames.map((v, i) => {
-                    return <p key={`${v.zipcode}${i}`}>{v.zipcode}</p>;
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return <p key={`${v.zipCode}${i}`}>{v.zipCode}</p>;
+                    })}
                 </div>
                 <div>
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     Toimipaikka
                   </TitleParagraph>
 
-                  {arrayOfNames.map((v, i) => {
-                    return <p key={`${v.city}${i}`}>{v.city}</p>;
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return <p key={`${v.city}${i}`}>{v.city}</p>;
+                    })}
                 </div>
                 <div>
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     Puhelinnro
                   </TitleParagraph>
 
-                  {arrayOfNames.map((v, i) => {
-                    return (
-                      <p key={`${v.phoneNumber}${i}`} className="mr-3 lg:mr-0">
-                        {v.phoneNumber}
-                      </p>
-                    );
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return (
+                        <p
+                          key={`${v.phoneNumber}${i}`}
+                          className="mr-3 lg:mr-0"
+                        >
+                          {v.phoneNumber}
+                        </p>
+                      );
+                    })}
                 </div>
                 <div>
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     S-posti
                   </TitleParagraph>
 
-                  {arrayOfNames.map((v, i) => {
-                    return (
-                      <p key={`${v.email}${i}`} className="mr-3 lg:mr-0">
-                        {v.email}
-                      </p>
-                    );
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return (
+                        <p key={`${v.email}${i}`} className="mr-3 lg:mr-0">
+                          {v.email}
+                        </p>
+                      );
+                    })}
                 </div>
                 <div>
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     Maksettu
                   </TitleParagraph>
-                  {arrayOfNames.map((v, i) => {
-                    return (
-                      <p key={`${v.paid}${i}`} className="mr-3 lg:mr-0">
-                        {v.paid}
-                      </p>
-                    );
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return (
+                        <p key={`${v.paid}${i}`} className="mr-3 lg:mr-0">
+                          {v.paid}
+                        </p>
+                      );
+                    })}
                 </div>
                 <div>
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     Liittymännro
                   </TitleParagraph>
 
-                  {arrayOfNames.map((v, i) => {
-                    return (
-                      <p key={`${v.connectionPointNumber}${i}`}>
-                        {v.connectionPointNumber}
-                      </p>
-                    );
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return (
+                        <p key={`${v.connectionPointNumber}${i}`}>
+                          {v.connectionPointNumber}
+                        </p>
+                      );
+                    })}
                 </div>
                 <div>
                   <TitleParagraph className="sticky top-0 mb-3 bg-gray-200">
                     Kommentit
                   </TitleParagraph>
 
-                  {arrayOfNames.map((v, i) => {
-                    return (
-                      <div key={`${v.comments}${i}`}>
-                        <p>-</p>
-                      </div>
-                    );
-                  })}
+                  {members &&
+                    members.map((v, i) => {
+                      return (
+                        <div key={`${v.comment}${i}`}>
+                          <p>-</p>
+                        </div>
+                      );
+                    })}
                 </div>
                 <div>
                   <TitleParagraph>Muokkaa/Poista</TitleParagraph>
-                  {arrayOfNames.map((v, i) => {
-                    return (
-                      <div key={`${v.comments}${i + 1}`}>
-                        <div className="relative">
-                          <button
-                            className="m-0 p-0"
-                            onClick={() => {
-                              console.log('Muokkaa:', arrayOfNames[i]);
-                            }}
-                          >
-                            <SvgWaterDrop
-                              width={18}
-                              height={18}
-                              className="bg-green-500"
-                            />
-                          </button>
-                          <button
-                            className="m-0 ml-2 p-0"
-                            onClick={() => {
-                              console.log('Poista:', arrayOfNames[i]);
-                            }}
-                          >
-                            <SvgWaterDrop
-                              width={18}
-                              height={18}
-                              className="bg-red-500"
-                            />
-                          </button>
+                  {members &&
+                    members.map((v, i) => {
+                      return (
+                        <div key={`${v.comment}${i + 1}`}>
+                          <div className="relative">
+                            <button
+                              className="m-0 p-0"
+                              onClick={() => {
+                                console.log('Muokkaa:', arrayOfNames[i]);
+                              }}
+                            >
+                              <SvgWaterDrop
+                                width={18}
+                                height={18}
+                                className="bg-green-500"
+                              />
+                            </button>
+                            <button
+                              className="m-0 ml-2 p-0"
+                              onClick={() => {
+                                console.log('Poista:', arrayOfNames[i]);
+                              }}
+                            >
+                              <SvgWaterDrop
+                                width={18}
+                                height={18}
+                                className="bg-red-500"
+                              />
+                            </button>
+                          </div>
                         </div>
-                      </div>
-                    );
-                  })}
+                      );
+                    })}
                 </div>
                 <button
                   className="absolute top-140 right-1/5 h-0 w-0"
