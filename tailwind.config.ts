8f7210628212184114f99a0432d7893115e9aa50
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2026-03-23 09:14:32 +0200

    Add tailwind.config.ts from lahjalista project

---
+++ tailwind.config.ts
@@ -0,0 +1,109 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgPage: '#FAF6F2',
+        bgForms: '#F7F7F5',
+        primaryDark: '#A3766B', // Rosa dark
+        primary: '#BC9288', // Rosa
+        primaryLight: '#ECD7D2', // Rosa light
+        secondaryDark: '#6A744C', // Green dark
+        secondary: '#6A744C', // Green
+        lines: '#594F47',
+        text: '#BC9288',
+        primaryText: '#404040',
+        textWhite: '#FFFFF',
+      },
+      keyframes: {
+        rainbowBackGroundColor: {
+          '0%': {
+            backgroundColor: 'red',
+          },
+          '10%': {
+            backgroundColor: 'orange',
+          },
+          '20%': {
+            backgroundColor: 'yellow',
+          },
+          '30%': {
+            backgroundColor: 'green',
+          },
+          '40%': {
+            backgroundColor: 'blue',
+          },
+          '50%': {
+            backgroundColor: 'indigo',
+          },
+          '60%': {
+            backgroundColor: 'violet',
+          },
+          '70%': {
+            backgroundColor: 'black',
+          },
+        },
+        width: {
+          '0%': {
+            width: '0px',
+          },
+          '100%': {
+            width: '100%',
+          },
+        },
+        highlight: {
+          '0%, 50%': {
+            filter: 'drop-shadow(0 0px 10px rgb(0 80 190 / 0.05))',
+          },
+          '25%, 75%': {
+            filter: 'drop-shadow(0px 0px 15px #269EEF)',
+          },
+        },
+        background: {
+          '0%': {
+            backgroundColor: 'yellow',
+          },
+          '100%': {
+            backgroundColor: 'blue',
+          },
+        },
+        opacity: {
+          '0%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '1',
+          },
+        },
+        pop: {
+          '0%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.4)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        highlight: 'highlight 1200ms ease-in-out',
+        rainbowBackGroundColor: 'rainbowBackGroundColor 1000ms',
+        background: 'background 1200ms ease-in-out',
+        width: 'width 1200ms',
+        opacity: 'opacity 800ms',
+        pop: 'pop 0.3s ease-out',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
