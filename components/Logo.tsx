565097546c0e92b54376653ba2f2050edc5d99be
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2026-07-30 13:28:06 +0300

    Add Logo.tsx for now

---
+++ components/Logo.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import { twMerge } from 'tailwind-merge';
+
+type Logo = {
+  wrapperClassName?: string;
+  imageClassName?: string;
+};
+
+export default function Logo({ wrapperClassName, imageClassName }: Logo) {
+  return (
+    <div className={twMerge('', wrapperClassName)}>
+      <Image
+        src={'/images/logo_with_text.svg'}
+        alt="Logo icon"
+        height={128}
+        width={128}
+        className={twMerge('', imageClassName)}
+      />
+    </div>
+  );
+}
