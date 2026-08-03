63b48b6a91db60b5ef63a5a6b7ab8dac24f72970
Antti <129953899+anttiasmala@users.noreply.github.com>
2026-08-03 18:12:01 +0300

    Update prisma/index.ts and prisma/schema.prisma

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,10 +1,13 @@
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 declare global {
   var prisma: undefined | PrismaClient;
 }
 
-const prisma = globalThis.prisma ?? new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma = globalThis.prisma ?? new PrismaClient({ adapter });
 
 export default prisma;
 
